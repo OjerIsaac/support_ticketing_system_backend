59fcc741b07f3797cca653c6190de60ae35a0ab7
OjerIsaac <isaacojerumu@gmail.com>
2025-06-28 22:09:45 +0100

    cleanup

--- app/graphql/types/user_type.rb
+++ app/graphql/types/user_type.rb
@@ -5,11 +5,8 @@ module Types
     field :id, ID, null: false
     field :name, String, null: false
     field :email, String, null: false
-    field :encrypted_password, String, null: false
-    field :role, Integer
-    field :reset_password_token, String
-    field :reset_password_sent_at, GraphQL::Types::ISO8601DateTime
-    field :remember_created_at, GraphQL::Types::ISO8601DateTime
+    field :role, String, null: false
+
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
     field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,37 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+users = [
+  {
+    email: "isaacojerumu@gmail.com",
+    password: "password123",
+    role: :agent,
+    name: "Isaac Ojerumu"
+  },
+  {
+    email: "ejiroebuka@gmail.com",
+    password: "password123",
+    role: :customer,
+    name: "Ejiro Ebuka"
+  },
+  {
+    email: "ejiroebuka+1@gmail.com",
+    password: "password123",
+    role: :customer,
+    name: "Customer 2"
+  }
+]
+
+users.each do |attrs|
+  user = User.find_or_initialize_by(email: attrs[:email])
+  if user.new_record?
+    user.password = attrs[:password]
+    user.password_confirmation = attrs[:password]
+    user.role = attrs[:role]
+    user.name = attrs[:name]
+    user.save!
+    puts "Created #{attrs[:role]}: #{attrs[:email]}"
+  else
+    puts "User already exists: #{attrs[:email]}"
+  end
+end

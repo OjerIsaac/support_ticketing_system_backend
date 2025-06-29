e1dd7201c8690cbdd3ce32e2de7c4357fa095eca
OjerIsaac <isaacojerumu@gmail.com>
2025-06-29 19:57:31 +0100

    cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,10 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "http://localhost:5173"
+     origins [
+      "http://localhost:5173",
+      "https://support-ticketing-system-frontend.onrender.com"
+    ]
 
     resource "*",
       headers: :any,
